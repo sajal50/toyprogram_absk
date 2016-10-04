a3421ba3a252d4e67fd4fe0f823471ded9f0fe74
Sajal Khandelwal <sajal50@gmail.com>
2016-10-04 00:28:07 -0400

    test cases for backend api

--- api/services/app/tests/ExampleTest.php
+++ api/services/app/tests/ExampleTest.php
@@ -7,11 +7,46 @@ class ExampleTest extends TestCase {
 	 *
 	 * @return void
 	 */
-	public function testBasicExample()
+	public function testGetAllTasks()
 	{
-		$crawler = $this->client->request('GET', '/');
+		$response = $this->call('GET','/tasks');
 
-		$this->assertTrue($this->client->getResponse()->isOk());
+		$responeArray = (array)$response->getData();
+		$flag = true;
+
+		foreach ($responeArray as $key => $value) {
+
+			if (!array_key_exists('task_id', $value) ) {
+
+				$flag = false;
+			} 
+		} 
+		
+
+
+
+        $this->assertEquals(true, $flag);
+
+		
+	}
+	public function testSetNewTask()
+	{
+		$response = $this->call('POST','/task', array('taskName' => 'test',  "taskAssignee" => 'testing' ));
+
+		$responeArray = (array)$response->getData();
+		$flag = true;
+
+		if (!array_key_exists('taskId', $responeArray)) {
+
+			$flag = false;
+
+		}		
+
+
+
+        $this->assertEquals(true, $flag);
+
+		
 	}
 
 }
